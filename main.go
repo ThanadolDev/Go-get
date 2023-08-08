d3c8f334511d9761ceba3db957ff574808412266
Thanadol <thanadol29.work@gmail.com>
2023-08-08 16:40:21 +0700

    init go
    trying out go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"item"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "por", Completed: true},
+	{ID: "2", Item: "por1", Completed: true},
+	{ID: "3", Item: "por2", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.Run("localhost:8080")
+}
